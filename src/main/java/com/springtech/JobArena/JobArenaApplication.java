ff2558bb42bf68b6cd9070769b751923b47991e0
Cumash <umashimec@gmail.com>
2024-04-13 03:26:25 +0100

    Job arena initialized

---
+++ src/main/java/com/springtech/JobArena/JobArenaApplication.java
@@ -0,0 +1,13 @@
+package com.springtech.JobArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobArenaApplication.class, args);
+	}
+
+}
